7955ba00abd5fe28f38ec7d8ed9e2bdb48f9787e
marco987 <pacemarco87@gmail.com>
2019-07-04 15:56:58 +0200

    Fine versione 1

--- script.js
+++ script.js
@@ -6,9 +6,21 @@
 // Esattamente quelle devono essere le stringhe di output.
 
 // Assegnazione variabili
-
+var num_min, num_max,
 
 // Elenco numeri da 1 a 100
-
+num_min = 1;
+num_max = 101;
 
 // Condizioni particolari
+for (num_min; num_min <= num_max; num_min++) {
+  if ((num_min % 15) == 1) {
+    console.log("FizzBuzz");
+  } else if ((num_min % 3) == 1) {
+    console.log("Fizz");
+  } else if ((num_min % 5) == 1) {
+    console.log("Buzz");
+  } else {
+    console.log(num_min - 1);
+  }
+}
